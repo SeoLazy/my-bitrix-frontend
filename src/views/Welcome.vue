edac4669b0972fd40ae1ee5cb8ac580c2793e890
sergej <seolazy@gmail.com>
2025-07-17 17:30:37 +0300

    2

---
+++ src/views/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="welcome">
+    <h1>Вы авторизованы</h1>
+    <p>Добро пожаловать, {{ name }} ({{ login }})</p>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const name = route.query.name || 'Гость'
+const login = route.query.login || 'неизвестен'
+</script>
